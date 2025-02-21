3b304f8d127cfbc2e63d345cd5d0989d54979262
Maksim Epishev <epishev.m@mail.ru>
2025-02-21 08:55:56 +0300

    feat: Add DetailedArgOutOfRangeException

---
+++ E314.Exceptions.Upm/Packages/com.e314.exceptions/Runtime/DetailedArgOutOfRangeException.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace E314.Exceptions
+{
+
+/// <summary>
+/// Represents an exception that provides detailed information about an argument being out of its valid range.
+/// This class extends <see cref="DetailedException"/> to include parameter-specific details and the actual value that caused the exception.
+/// </summary>
+[Serializable]
+public class DetailedArgOutOfRangeException : DetailedException
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DetailedArgOutOfRangeException"/> class with a specified parameter name, actual value, error message, error code, and additional error data.
+	/// </summary>
+	/// <param name="paramName">The name of the parameter that caused the exception.</param>
+	/// <param name="actualValue">The actual value of the parameter that was out of range.</param>
+	/// <param name="message">The error message that explains the reason for the exception. Default is "Specified argument was out of the range of valid values.".</param>
+	/// <param name="errorCode">A unique error code associated with the exception. Default is "ARG_OUT_OF_RANGE".</param>
+	/// <param name="errorData">Additional data related to the error. Default is null.</param>
+	/// <param name="fileName">The full path of the source file that contains the caller. Automatically populated by the runtime.</param>
+	/// <param name="methodName">The name of the method or property that invoked the exception. Automatically populated by the runtime.</param>
+	/// <param name="lineNumber">The line number in the source file at which the exception was thrown. Automatically populated by the runtime.</param>
+	public DetailedArgOutOfRangeException(
+		string paramName,
+		object actualValue,
+		string message = "Specified argument was out of the range of valid values.",
+		string errorCode = "ARG_OUT_OF_RANGE",
+		object errorData = null,
+		[CallerFilePath] string fileName = "",
+		[CallerMemberName] string methodName = "",
+		[CallerLineNumber] int lineNumber = 0)
+		: base(
+			$"{message} (Parameter '{paramName}' with value '{actualValue}')",
+			errorCode,
+			errorData,
+			fileName,
+			methodName,
+			lineNumber)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DetailedArgOutOfRangeException"/> class with serialized data.
+	/// </summary>
+	/// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+	/// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+	protected DetailedArgOutOfRangeException(SerializationInfo info, StreamingContext context)
+		: base(info, context)
+	{
+	}
+}
+
+}
---
+++ E314.Exceptions.Upm/Packages/com.e314.exceptions/Tests/DetailedArgOutOfRangeExceptionTests.cs
@@ -0,0 +1,119 @@
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace E314.Exceptions.Tests
+{
+
+[TestFixture]
+internal sealed class DetailedArgOutOfRangeExceptionTests
+{
+	private const string TestParamName = "testParam";
+	private const int TestActualValue = 150;
+	private const string TestMessage = "Specified argument was out of the range of valid values.";
+	private const string TestErrorCode = "ARG_OUT_OF_RANGE";
+	private const string TestFileName = "TestFile.cs";
+	private const string TestMethodName = "TestMethod";
+	private const int TestLineNumber = 42;
+	private readonly object _testErrorData = new { MinValue = 0, MaxValue = 100 };
+
+	[Test]
+	public void Constructor_WithAllParameters_SetsPropertiesCorrectly()
+	{
+		// Arrange & Act
+		var exception = new DetailedArgOutOfRangeException(
+			TestParamName,
+			TestActualValue,
+			TestMessage,
+			TestErrorCode,
+			_testErrorData,
+			TestFileName,
+			TestMethodName,
+			TestLineNumber);
+
+		// Assert
+		Assert.That(exception.Message,
+			Does.Contain(TestMessage).And.Contain(TestParamName).And
+				.Contain(TestActualValue.ToString()));
+		Assert.That(exception.ErrorCode, Is.EqualTo(TestErrorCode));
+		Assert.That(exception.ErrorData, Is.EqualTo(_testErrorData));
+		Assert.That(exception.FileName, Is.EqualTo(TestFileName));
+		Assert.That(exception.MethodName, Is.EqualTo(TestMethodName));
+		Assert.That(exception.LineNumber, Is.EqualTo(TestLineNumber));
+	}
+
+	[Test]
+	public void Constructor_WithDefaultValues_SetsPropertiesCorrectly()
+	{
+		// Arrange & Act
+		var exception = new DetailedArgOutOfRangeException(TestParamName, TestActualValue);
+
+		// Assert
+		Assert.That(exception.Message,
+			Does.Contain("Specified argument was out of the range of valid values.").And
+				.Contain(TestParamName).And.Contain(TestActualValue.ToString()));
+		Assert.That(exception.ErrorCode, Is.EqualTo("ARG_OUT_OF_RANGE"));
+		Assert.That(exception.ErrorData, Is.Null);
+		Assert.That(exception.FileName, Is.Not.Null.And.Not.Empty);
+		Assert.That(exception.MethodName, Is.Not.Null.And.Not.Empty);
+		Assert.That(exception.LineNumber, Is.GreaterThan(0));
+	}
+
+	[Test]
+	public void Serialization_Deserialization_RestoresPropertiesCorrectly()
+	{
+		// Arrange
+		var originalException = new DetailedArgOutOfRangeException(
+			TestParamName,
+			TestActualValue,
+			TestMessage,
+			TestErrorCode,
+			_testErrorData,
+			TestFileName,
+			TestMethodName,
+			TestLineNumber);
+
+		string json = JsonConvert.SerializeObject(originalException);
+		var deserializedException = JsonConvert.DeserializeObject<DetailedException>(json);
+		string jsonErrorData = JsonConvert.SerializeObject(deserializedException.ErrorData);
+		string errorData = JsonConvert.SerializeObject(_testErrorData);
+
+		// Assert
+		Assert.That(deserializedException.Message,
+			Does.Contain(TestMessage).And.Contain(TestParamName).And
+				.Contain(TestActualValue.ToString()));
+		Assert.That(deserializedException.ErrorCode, Is.EqualTo(TestErrorCode));
+		Assert.That(jsonErrorData, Is.EqualTo(errorData));
+		Assert.That(deserializedException.FileName, Is.EqualTo(TestFileName));
+		Assert.That(deserializedException.MethodName, Is.EqualTo(TestMethodName));
+		Assert.That(deserializedException.LineNumber, Is.EqualTo(TestLineNumber));
+	}
+
+	[Test]
+	public void ToString_IncludesAllDetails()
+	{
+		// Arrange
+		var exception = new DetailedArgOutOfRangeException(
+			TestParamName,
+			TestActualValue,
+			TestMessage,
+			TestErrorCode,
+			_testErrorData,
+			TestFileName,
+			TestMethodName,
+			TestLineNumber);
+
+		// Act
+		var toStringResult = exception.ToString();
+
+		// Assert
+		Assert.That(toStringResult, Does.Contain(TestMessage));
+		Assert.That(toStringResult, Does.Contain(TestErrorCode));
+		Assert.That(toStringResult, Does.Contain(_testErrorData.ToString()));
+		Assert.That(toStringResult, Does.Contain(TestFileName));
+		Assert.That(toStringResult, Does.Contain(TestMethodName));
+		Assert.That(toStringResult, Does.Contain(TestLineNumber.ToString()));
+		Assert.That(toStringResult, Does.Contain(TestActualValue.ToString()));
+	}
+}
+
+}
